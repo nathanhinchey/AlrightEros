d461416f1a33307c1f66e4c86f79f99f0f5717f9
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-12 10:55:21 -0400

    Create User model in ruby

--- okcupidclone/app/models/user.rb
+++ okcupidclone/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
+  #TODO: validate email by regex
+  validates :password_digest, presence: true
+
+  has_many :session_tokens
 end
