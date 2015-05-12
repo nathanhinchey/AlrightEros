29d741ace66ef0e27fa9cecf2671788d3aa70697
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-12 11:28:43 -0400

    Fix error in authentic_form helper method.

--- okcupidclone/app/helpers/application_helper.rb
+++ okcupidclone/app/helpers/application_helper.rb
@@ -1,7 +1,5 @@
 module ApplicationHelper
   def authentic_form
-    <<-HTML.html_safe
-      <input name="authenticity_token" value="<%= form_authenticity_token %>" type="hidden">
-    HTML
+    "<input name=\"authenticity_token\" value=\"#{form_authenticity_token}\" type=\"hidden\">".html_safe
   end
 end
