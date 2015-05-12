22cb84d426de841db0340f91df8c3dfa8cbe10b5
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-12 14:25:33 -0400

    Limit users and sessions routes to new, create, destroy

--- okcupidclone/config/routes.rb
+++ okcupidclone/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :sessions
+  resources :users, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
