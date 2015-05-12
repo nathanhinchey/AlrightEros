687037c1b2e256204a4ee878f31e3f44f6730409
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-12 12:19:15 -0400

    Switch to backbone-on-rails gem; remembering to use --javascript flag when installing

---
+++ okcupidclone/app/assets/javascripts/okcupidclone.js
@@ -0,0 +1,12 @@
+window.Okcupidclone = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  Okcupidclone.initialize();
+});
