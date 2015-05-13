875e6bd223ebdac9b3596faf7982dcfd6e55f2c8
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-13 11:49:50 -0400

    Add validation for age to profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,9 @@
 class Profile < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
+  validates :birthday, presence: true
 
   validate :must_have_at_least_one_gender
+  validate :must_be_adult
 
   has_many :profile_genders, dependent: :destroy
   has_many :genders, through: :profile_genders, source: :genders
