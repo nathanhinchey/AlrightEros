675ab225ca330a6bf3d645d2dae5dc860505b30a
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-14 10:40:45 -0400

    Add before_filter so site can only be used while logged in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController
+  before_filter :require_signed_in
+
   def index
     render :index
   end
+
+  private
+    def require_signed_in
+      redirect_to new_user_url unless current_user
+    end
 end
