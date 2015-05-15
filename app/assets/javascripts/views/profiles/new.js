4b6873b68b79e2c321f085397c6d5bf0bfed251c
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-15 18:18:41 -0400

    Create new profile view for Backbone

---
+++ app/assets/javascripts/views/profiles/new.js
@@ -0,0 +1,35 @@
+AlrightEros.Views.ProfileNew = Backbone.View.extend({
+
+  initialize: function (options) {
+    this.listenTo(this.model, "sync", this.render);
+  },
+
+  events: {
+    'submit form': 'submit'
+  },
+
+  template: JST['profiles/new'],
+
+  render: function () {
+    this.$el.html(this.template());
+
+    return this;
+  },
+
+  submit: function (event) {
+    event.preventDefault();
+
+    var $form = $(event.currentTarget);
+    var formData = $form.serializeJSON().profile;
+
+    this.model.set(formData);
+    var profile = this.model;
+
+    profile.save({},{
+      success: function () {
+        AlrightEros.profiles.add(profile);
+        Backbone.history.navigate("", {trigger: true});
+      }
+    });
+  },
+});
