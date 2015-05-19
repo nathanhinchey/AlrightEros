13572c554137992a32062b4f559db128bbf89369
Nathan Hinchey <nathanhinchey@gmail.com>
2015-05-19 16:52:42 -0400

    Correct possible bug in question loop.
    I was relying on no questions ever being deleted. Now I'm not.

--- app/assets/javascripts/collections/questions.js
+++ app/assets/javascripts/collections/questions.js
@@ -4,19 +4,19 @@ AlrightEros.Collections.Questions = Backbone.Collection.extend({
   model: AlrightEros.Models.Question,
 
   nextNewQuestion: function (startNum) {
-    (startNum += 1) || (startNum = 1);
+    (startNum += 1) || (startNum = 0);
 
     var answered_questions = AlrightEros.currentUser.attributes.answered_questions
     for (var modelIdx = startNum; modelIdx < this.models.length; modelIdx++){
       if (_.indexOf(answered_questions, modelIdx) === -1 ){
-        return modelIdx;
+        return this.models[modelIdx].id;
       }
     }
 
-    if (startNum !== 1) {
-      for (var modelIdx = 1; modelIdx < startNum; modelIdx++){
+    if (startNum !== 0) {
+      for (var modelIdx = 0; modelIdx < startNum; modelIdx++){
         if (_.indexOf(answered_questions, modelIdx) === -1 ){
-          return modelIdx;
+          return this.models[modelIdx].id;
         }
       }
     }
